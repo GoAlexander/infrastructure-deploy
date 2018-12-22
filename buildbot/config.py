085dc6b747061e9e0f2162fdf117c4ea0cc12674
Alexander Dydychkin <AlexanderDydychkin@yandex.ru>
2018-12-23 00:35:17 +0300

    Update and rename config.py to buildbot/config.py

--- buildbot/config.py
+++ buildbot/config.py
@@ -1,5 +1,4 @@
 import sys
-from enum import Enum
 
 import project_secrets
 
@@ -7,23 +6,21 @@ BUILD = "build"
 BUILD_MASTER = "build-master-branch"
 TEST = "test"
 
-#RUN_COMMAND = "python3"
 WORKERS = {
-    BUILD: {"b-1": {},
+    BUILD: {"example-worker": {},
             "b-2": {}},
     TEST: {"t-1": {},
            "t-2": {}}
 }
 
-PORT = "5000"
+PORT = "8010"
 WORKER_PORT = "9000"
 BUILDBOT_NET_USAGE_DATA = None # "None" disables the sending of usage analysis info to buildbot.net
 BUILDBOT_TREE_STABLE_TIMER = None # Value "None" means that a separate build will be started immediately for each Change.
-BUILDBOT_TITLE = "Hello, World"
+BUILDBOT_TITLE = "Hello, World!"
 POLL_INTERVAL = 10 # Poll Github for new changes (in seconds)
 
-WORKER_PASS = project_secrets.WORKER_PASS
-DATABASE_PASSWORD = project_secrets.DATABASE_PASSWORD
+WORKER_PASS = project_secrets.WORKER_PASS #default "pass"
 GITHUB_TOKEN = project_secrets.GITHUB_TOKEN
 GITHUB_OWNER = "GoAlexander"
 GITHUB_OWNERS_REPO = "infrastructure-test"
